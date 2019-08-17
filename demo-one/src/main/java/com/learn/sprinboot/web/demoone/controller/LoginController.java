46771aef09c0c2599a5b0f325da0e5f782c89fa6
RONIT CHATTOPADHYAY <ronit@yourdost.com>
2019-08-17 16:03:20 +0530

    JSP View Added

--- demo-one/src/main/java/com/learn/sprinboot/web/demoone/controller/LoginController.java
+++ demo-one/src/main/java/com/learn/sprinboot/web/demoone/controller/LoginController.java
@@ -1,13 +1,22 @@
 package com.learn.sprinboot.web.demoone.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 
     @RequestMapping("/login")
-    public String loginMessage(){
-        return "Hello World";
+    public String loginMessage(@RequestParam String name, ModelMap model){
+        if(name == null || name.equals("")){
+            model.put("name", "Boss");
+        }else{
+            model.put("name", name);
+        }
+
+        return "login";
     }
 }
---
+++ demo-one/src/main/webapp/WEB-INF/jsp/login.jsp
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Demo-1</title>
+    </head>
+
+    <body>
+        Demo 1 First JSP!!! Welcome ${name}
+    </body>
+</html
